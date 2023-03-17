8e712ea29f9c88718debebf2729e9d637ceaaf73
BoraOzkoc <48334969+BoraOzkoc@users.noreply.github.com>
2023-03-17 23:09:55 +0300

    Level Buttons added & level brain added

--- Assets/LevelButtonManager.cs
+++ Assets/LevelButtonManager.cs
@@ -1,16 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LevelButtonManager : MonoBehaviour
 {
     [SerializeField] private List<LevelButtonProperties> levelList = new List<LevelButtonProperties>();
 
+    public void OnEnable()
+    {
+        Init();
+    }
+
     public void Init()
     {
         for (int i = 0; i < levelList.Count; i++)
         {
-            levelList[i].Init(i);
+            levelList[i].Init(i + 1);
         }
     }
 }
